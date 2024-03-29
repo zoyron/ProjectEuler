1a2c474988d684e1dcd075042d2dd783abc4ba42
zoyron <sgrarora3@gmail.com>
2024-03-29 14:47:58 +0530

    added the solution to 10th

---
+++ 1-10/10.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define lli long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl '\n'
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define N 2000001
+
+using namespace std;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+bool sieve[N];
+
+void solve(){
+  memset(sieve, 1, N);
+  lli sum = 0;
+  sieve[0] = 0;
+  sieve[1] = 0;
+  int i, j;
+  for(i = 0; i < N ; i++){
+    if(sieve[i]){
+      sum += i;
+      for(j = i+i; j < N; j += i)
+        sieve[j] = 0;
+    }
+  }
+  cout << sum << endl;
+}
+
+int32_t main(){
+  int t = 1;
+  //cin >> t;
+  while(t--) solve();
+  return 0;
+}
+
